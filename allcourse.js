31d7f2ac3578e8905205d12dfcee701a708ec86e
shihab4513 <shihab4513@gmail.com>
2024-04-16 17:02:10 +0600

    header re-colored

--- allcourse.js
+++ allcourse.js
@@ -1,85 +1,87 @@
 function makePurple(element) {
-   const x= document.getElementById='element';
-   x.style.backgroundColor='pink';
+    const x= document.getElementById='element';
+    x.style.backgroundColor='pink';
+    
+     
+ }
+ function removeHidden(){
+     
+ }
+ document.getElementById('designButton').addEventListener('click', function () {
+     const button = document.getElementById('designButton');
+     const dSec=document.getElementById('design');
+     dSec.classList.remove('hidden');
    
-    
-}
-function removeHidden(){
-    
-}
-document.getElementById('designButton').addEventListener('click', function () {
-    const button = document.getElementById('designButton');
-    const dSec=document.getElementById('design');
-    dSec.classList.remove('hidden');
-  
-    const devSec=document.getElementById('development');
-    const bsSec=document.getElementById('buisness');
-    const dataSec=document.getElementById('Data-Science'); 
-    if(!devSec.classList.contains('hidden'))
-    {
-       devSec.classList.add('hidden');
-    }
-    else if(!bsSec.classList.contains('hidden'))
-    {
-        bsSec.classList.add('hidden');
-    }
-    else if(!dataSec.classList.contains('hidden'))
-    {
-        dataSec.classList.add('hidden');
-    }
-  
+     const devSec=document.getElementById('development');
+     const bsSec=document.getElementById('buisness');
+     const dataSec=document.getElementById('Data-Science'); 
+     if(!devSec.classList.contains('hidden'))
+     {
+        devSec.classList.add('hidden');
+     }
+     else if(!bsSec.classList.contains('hidden'))
+     {
+         bsSec.classList.add('hidden');
+     }
+     else if(!dataSec.classList.contains('hidden'))
+     {
+         dataSec.classList.add('hidden');
+     }
    
-    // dSec.classList.remove('hidden');
+    const y=document.getElementById('allFooter');
+    y.classList.remove('hidden');
     
-
-});
-document.getElementById('developmentButton').addEventListener('click', function () {
-    const button = document.getElementById('developmentButton');
-    const devSec=document.getElementById('development');
-    devSec.classList.remove('hidden');
+     
+ 
+ });
+ document.getElementById('developmentButton').addEventListener('click', function () {
+     const button = document.getElementById('developmentButton');
+     const devSec=document.getElementById('development');
+     devSec.classList.remove('hidden');
+     
+     
+     const dSec=document.getElementById('design');
     
-    
-    const dSec=document.getElementById('design');
-   
-    
-    
-    const bsSec=document.getElementById('buisness');
-    const dataSec=document.getElementById('Data-Science'); 
-    if(!dSec.classList.contains('hidden'))
-    {
-       dSec.classList.add('hidden');
-    }
-    else if(!bsSec.classList.contains('hidden'))
-    {
-        bsSec.classList.add('hidden');
-    }
-    else if(!dataSec.classList.contains('hidden'))
-    {
-        dataSec.classList.add('hidden');
-    }
-    
-
-})
-// document.getElementById('designButton').addEventListener('click', function () {
-//     const button = document.getElementById('designButton');
-    
-//     const dSec=document.getElementById('design');
-//     // dSec.classList.remove('hidden');
-//     dSec.classList.remove('hidden');
-//     const devSec=document.getElementById('development');
-//     const bsSec=document.getElementById('buisness');
-//     const dataSec=document.getElementById('Data-Science'); 
-//     if(!devSec.classList.contains('hidden'))
-//     {
-//        devSec.classList.add('hidden');
-//     }
-//     else if(!bsSec.classList.contains('hidden'))
-//     {
-//         bsSec.classList.add('hidden');
-//     }
-//     else if(!dataSec.classList.contains('hidden'))
-//     {
-//         dataSec.classList.add('hidden');
-//     }
-
-// })
+     
+     
+     const bsSec=document.getElementById('buisness');
+     const dataSec=document.getElementById('Data-Science'); 
+     if(!dSec.classList.contains('hidden'))
+     {
+        dSec.classList.add('hidden');
+     }
+     else if(!bsSec.classList.contains('hidden'))
+     {
+         bsSec.classList.add('hidden');
+     }
+     else if(!dataSec.classList.contains('hidden'))
+     {
+         dataSec.classList.add('hidden');
+     }
+     
+ 
+ })
+ // document.getElementById('designButton').addEventListener('click', function () {
+ //     const button = document.getElementById('designButton');
+     
+ //     const dSec=document.getElementById('design');
+ //     // dSec.classList.remove('hidden');
+ //     dSec.classList.remove('hidden');
+ //     const devSec=document.getElementById('development');
+ //     const bsSec=document.getElementById('buisness');
+ //     const dataSec=document.getElementById('Data-Science'); 
+ //     if(!devSec.classList.contains('hidden'))
+ //     {
+ //        devSec.classList.add('hidden');
+ //     }
+ //     else if(!bsSec.classList.contains('hidden'))
+ //     {
+ //         bsSec.classList.add('hidden');
+ //     }
+ //     else if(!dataSec.classList.contains('hidden'))
+ //     {
+ //         dataSec.classList.add('hidden');
+ //     }
+ 
+ // })
+ 
